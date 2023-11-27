cdc426197e3e86e65f0395021fd0def865216b4a
Mirna Gama <mirnabeatrizgamav@gmail.com>
2023-11-26 21:38:31 -0300

    [R1] Add user DTO class

---
+++ src/main/java/com/mirna/transferapi/domain/dtos/UserDTO.java
@@ -0,0 +1,59 @@
+package com.mirna.transferapi.domain.dtos;
+
+import java.math.BigDecimal;
+
+public class UserDTO {
+
+	private String email;
+	private String password;
+	private String firstName;
+	private String lastName;
+	private String document;
+	private BigDecimal balance;
+	private String userType;
+	
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getDocument() {
+		return document;
+	}
+	public void setDocument(String document) {
+		this.document = document;
+	}
+	public BigDecimal getBalance() {
+		return balance;
+	}
+	public void setBalance(BigDecimal balance) {
+		this.balance = balance;
+	}
+	public String getUserType() {
+		return userType;
+	}
+	public void setUserType(String userType) {
+		this.userType = userType;
+	}
+	
+	
+}
