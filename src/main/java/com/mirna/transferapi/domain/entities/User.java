393fdb7877c164b001ed597864a25d6ffc2d33eb
Mirna Gama <mirnabeatrizgamav@gmail.com>
2023-11-26 21:36:51 -0300

    [R1] Create user entity

---
+++ src/main/java/com/mirna/transferapi/domain/entities/User.java
@@ -0,0 +1,108 @@
+package com.mirna.transferapi.domain.entities;
+
+import java.math.BigDecimal;
+
+import com.mirna.transferapi.domain.enums.UserType;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity(name="users")
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name="first_name")
+	private String firstName;
+	
+	@Column(name="last_name")
+	private String lastName;
+	
+	@Column(name="email", unique=true)
+	private String email;
+	
+	@Column(name="password")
+	private String password;
+	
+	@Column(name="document", unique=true)
+	private String document;
+	
+	@Column(name="balance")
+	private BigDecimal balance;
+	
+	@Enumerated(EnumType.STRING)
+	@Column(name="user_type")
+	private UserType userType;
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public BigDecimal getBalance() {
+		return balance;
+	}
+
+	public void setBalance(BigDecimal balance) {
+		this.balance = balance;
+	}
+
+	public UserType getUserType() {
+		return userType;
+	}
+
+	public void setUserType(UserType userType) {
+		this.userType = userType;
+	}
+
+	public String getDocument() {
+		return document;
+	}
+
+	public void setDocument(String document) {
+		this.document = document;
+	}	
+	
+}
