3d9e46436d1aeabf370126c644cfe03b18117ffd
Mirna Gama <mirnabeatrizgamav@gmail.com>
2023-12-09 18:45:15 -0300

    [R1] Remove exception handler methods because they don't work with unit tests

--- src/main/java/com/mirna/transferapi/controllers/UserController.java
+++ src/main/java/com/mirna/transferapi/controllers/UserController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mirna.transferapi.domain.dtos.UserDTO;
@@ -20,39 +21,45 @@ import com.mirna.transferapi.exceptions.UserDocumentException;
 import com.mirna.transferapi.exceptions.UserEmailException;
 import com.mirna.transferapi.services.UserService;
 
+import jakarta.servlet.http.HttpServletRequest;
+
 @RestController
 @RequestMapping("/v1/users")
 public class UserController {
-    
+
 	@Autowired
-    private UserService userService;
+	private UserService userService;
 
-    @RequestMapping(method = RequestMethod.POST)
-    public UserDTO addUser(@RequestBody UserDTO userDTO) throws UserEmailException, UserDocumentException {
-    	return userService.addUser(userDTO);
-    }
-    
-    @RequestMapping(value = "/{document}", method = RequestMethod.GET)
-    public ResponseEntity<User> getUser(@PathVariable("document") String document) throws EntityNotPresentException {
-        
-       User user = userService.fetchUser(document);
-       return ResponseEntity.ok(user);
-    }
-    
-    @ExceptionHandler(EntityNotPresentException.class)
-    public ResponseEntity<Object> handleEntityNotPresentException(EntityNotPresentException exception) {
-        Map<String, Object> body = new HashMap<>();
-        body.put("message", exception.getMessage());
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Object> addUser(@RequestBody UserDTO userDTO) {
 
-        return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
-    }
-    
-    @ExceptionHandler({UserEmailException.class, UserDocumentException.class})
-    public ResponseEntity<Object> handleUserEmailException(Exception exception) {
-        Map<String, Object> body = new HashMap<>();
-        body.put("message", exception.getMessage());
+		UserDTO result = null;
+		try {
+			result = userService.addUser(userDTO);
+		} catch (UserEmailException | UserDocumentException userException) {
+			Map<String, Object> body = new HashMap<>();
+			body.put("message", userException.getMessage());
 
-        return new ResponseEntity<>(body, HttpStatus.UNPROCESSABLE_ENTITY);
-    }
-    
+			return new ResponseEntity<>(body, HttpStatus.UNPROCESSABLE_ENTITY);
+		}
+
+		return ResponseEntity.ok(result);
+	}
+
+	@RequestMapping(value = "/{document}", method = RequestMethod.GET)
+	public ResponseEntity<Object> getUser(@PathVariable("document") String document) {
+
+		User user = null;
+		try {
+			user = userService.fetchUser(document);
+		} catch (EntityNotPresentException entityNotPresentException) {
+			Map<String, Object> body = new HashMap<>();
+			body.put("message", entityNotPresentException.getMessage());
+
+			return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
+		}
+
+		return ResponseEntity.ok(user);
+	}
+
 }
