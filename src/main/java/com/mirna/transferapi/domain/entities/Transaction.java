6c228c2d6012e4949e81368d268b9c282adb8c2b
Mirna Gama <mirnabeatrizgamav@gmail.com>
2023-12-09 22:42:32 -0300

    [R2] Create transaction entity

---
+++ src/main/java/com/mirna/transferapi/domain/entities/Transaction.java
@@ -0,0 +1,74 @@
+package com.mirna.transferapi.domain.entities;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity(name = "transactions")
+public class Transaction {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne
+	@JoinColumn(name = "sender_id")
+	private User sender;
+
+	@ManyToOne
+	@JoinColumn(name = "receiver_id")
+	private User receiver;
+
+	@Column(name = "amount")
+	private BigDecimal amount;
+
+	@Column(name = "timestamp")
+	private LocalDateTime timestamp;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public User getSender() {
+		return sender;
+	}
+
+	public void setSender(User sender) {
+		this.sender = sender;
+	}
+
+	public User getReceiver() {
+		return receiver;
+	}
+
+	public void setReceiver(User receiver) {
+		this.receiver = receiver;
+	}
+
+	public BigDecimal getAmount() {
+		return amount;
+	}
+
+	public void setAmount(BigDecimal amount) {
+		this.amount = amount;
+	}
+
+	public LocalDateTime getTimestamp() {
+		return timestamp;
+	}
+
+	public void setTimestamp(LocalDateTime timestamp) {
+		this.timestamp = timestamp;
+	}
+}
