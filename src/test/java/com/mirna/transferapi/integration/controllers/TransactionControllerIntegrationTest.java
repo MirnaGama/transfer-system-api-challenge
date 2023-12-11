c9dfbf4515d89f1582cfe31a16d64c76e8124c01
Mirna Gama <mirnabeatrizgamav@gmail.com>
2023-12-11 17:58:39 -0300

    [R3] Apply the user type scenario for integration tests

--- src/test/java/com/mirna/transferapi/integration/controllers/TransactionControllerIntegrationTest.java
+++ src/test/java/com/mirna/transferapi/integration/controllers/TransactionControllerIntegrationTest.java
@@ -12,7 +12,9 @@ import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -21,8 +23,10 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mirna.transferapi.domain.dtos.TransactionDTO;
+import com.mirna.transferapi.domain.entities.Transaction;
 import com.mirna.transferapi.domain.entities.User;
 import com.mirna.transferapi.domain.enums.UserType;
+import com.mirna.transferapi.exceptions.SenderUserTypeInvalidException;
 import com.mirna.transferapi.repositories.TransactionRepository;
 import com.mirna.transferapi.repositories.UserRepository;
 import com.mirna.transferapi.security.auth.util.PasswordEncryptorUtil;
@@ -65,8 +69,18 @@ public class TransactionControllerIntegrationTest {
 		user2.setPassword(PasswordEncryptorUtil.encryptPassword("p4ss0wrd"));
 		user2.setUserType(UserType.COMMON);
 
+		User user3 = new User();
+		user3.setEmail("store123@gmail.com");
+		user3.setFirstName("Store");
+		user3.setLastName("Test");
+		user3.setDocument("333333333");
+		user3.setBalance(new BigDecimal(1000));
+		user3.setPassword(PasswordEncryptorUtil.encryptPassword("p4ss0wrd"));
+		user3.setUserType(UserType.SHOPKEEPER);
+
 		userRepository.save(user);
 		userRepository.save(user2);
+		userRepository.save(user3);
 	}
 	
 	@AfterAll
@@ -99,7 +113,7 @@ public class TransactionControllerIntegrationTest {
 		TransactionDTO transactionDTO = new TransactionDTO();
 		transactionDTO.setAmount(new BigDecimal(100));
 		transactionDTO.setReceiverDocument("111111111");
-		transactionDTO.setSenderDocument("3333333333");
+		transactionDTO.setSenderDocument("4444444444");
 
 		String transactionDTOContent = mapper.writeValueAsString(transactionDTO);
 
@@ -107,6 +121,22 @@ public class TransactionControllerIntegrationTest {
 				.characterEncoding("UTF-8").content(transactionDTOContent))
 				.andExpect(MockMvcResultMatchers.status().isNotFound()).andDo(MockMvcResultHandlers.print());
 
+	}
+	
+	@Test
+	@DisplayName("Should return http status unprocessable entity when adding transiction with sender with invalid user type")
+	public void testAddTransactionSenderUserTypeInvalidFailure() throws Exception {
+
+		TransactionDTO transactionDTO = new TransactionDTO();
+		transactionDTO.setAmount(new BigDecimal(100));
+		transactionDTO.setReceiverDocument("111111111");
+		transactionDTO.setSenderDocument("333333333");
+
+		String transactionDTOContent = mapper.writeValueAsString(transactionDTO);
+
+		mockMvc.perform(MockMvcRequestBuilders.post("/v1/transactions").contentType(MediaType.APPLICATION_JSON)
+				.characterEncoding("UTF-8").content(transactionDTOContent))
+				.andExpect(MockMvcResultMatchers.status().isUnprocessableEntity()).andDo(MockMvcResultHandlers.print());
 	}
 
 }
--- src/test/java/com/mirna/transferapi/integration/services/TransactionServiceIntegrationTest.java
+++ src/test/java/com/mirna/transferapi/integration/services/TransactionServiceIntegrationTest.java
@@ -23,6 +23,7 @@ import com.mirna.transferapi.domain.entities.Transaction;
 import com.mirna.transferapi.domain.entities.User;
 import com.mirna.transferapi.domain.enums.UserType;
 import com.mirna.transferapi.exceptions.EntityNotPresentException;
+import com.mirna.transferapi.exceptions.SenderUserTypeInvalidException;
 import com.mirna.transferapi.repositories.TransactionRepository;
 import com.mirna.transferapi.repositories.UserRepository;
 import com.mirna.transferapi.security.auth.util.PasswordEncryptorUtil;
@@ -36,13 +37,13 @@ public class TransactionServiceIntegrationTest {
 
 	@Autowired
 	private TransactionService transactionService;
-	
+
 	@Autowired
 	private TransactionRepository transactionRepository;
-	
+
 	@Autowired
 	private UserRepository userRepository;
-	
+
 	@BeforeAll
 	public void init() {
 		User user = new User();
@@ -63,60 +64,90 @@ public class TransactionServiceIntegrationTest {
 		user2.setPassword(PasswordEncryptorUtil.encryptPassword("p4ss0wrd"));
 		user2.setUserType(UserType.COMMON);
 
+		User user3 = new User();
+		user3.setEmail("store123@gmail.com");
+		user3.setFirstName("Store");
+		user3.setLastName("Test");
+		user3.setDocument("333333333");
+		user3.setBalance(new BigDecimal(1000));
+		user3.setPassword(PasswordEncryptorUtil.encryptPassword("p4ss0wrd"));
+		user3.setUserType(UserType.SHOPKEEPER);
+
 		userRepository.save(user);
 		userRepository.save(user2);
+		userRepository.save(user3);
 	}
-	
+
 	@AfterAll
 	public void terminate() {
 		transactionRepository.deleteAll();
 		userRepository.deleteAll();
 	}
-	
+
 	@Test
 	@DisplayName("Should add transaction successfully")
 	public void testAddTransaction() throws Exception {
-		
+
 		TransactionDTO transactionDTO = new TransactionDTO();
 		transactionDTO.setAmount(new BigDecimal(500));
 		transactionDTO.setSenderDocument("222222222");
 		transactionDTO.setReceiverDocument("111111111");
 
-	    User initialSender = userRepository.findUserByDocument(transactionDTO.getSenderDocument()).get();
-	    User initialReceiver = userRepository.findUserByDocument(transactionDTO.getReceiverDocument()).get();
-	    
-	    assertThat(initialSender).isNotNull();
-	    assertThat(initialReceiver).isNotNull();
-	    
-	    Transaction transaction = transactionService.addTransaction(transactionDTO);
+		User initialSender = userRepository.findUserByDocument(transactionDTO.getSenderDocument()).get();
+		User initialReceiver = userRepository.findUserByDocument(transactionDTO.getReceiverDocument()).get();
 
-	    assertThat(transaction.getId()).isNotNull();
-	    
-	    BigDecimal senderFinalBalance = initialSender.getBalance().subtract(transaction.getAmount());
-	    BigDecimal receiverFinalBalance = initialReceiver.getBalance().add(transaction.getAmount());
-	    
-	    User sender = transaction.getSender();
-	    User receiver = transaction.getReceiver();
-	    
-	    assertTrue(sender.getBalance().compareTo(senderFinalBalance) == 0);
-	    assertTrue(receiver.getBalance().compareTo(receiverFinalBalance) == 0);   
+		assertThat(initialSender).isNotNull();
+		assertThat(initialReceiver).isNotNull();
+
+		Transaction transaction = transactionService.addTransaction(transactionDTO);
+
+		assertThat(transaction.getId()).isNotNull();
+
+		BigDecimal senderFinalBalance = initialSender.getBalance().subtract(transaction.getAmount());
+		BigDecimal receiverFinalBalance = initialReceiver.getBalance().add(transaction.getAmount());
+
+		User sender = transaction.getSender();
+		User receiver = transaction.getReceiver();
+
+		assertTrue(sender.getBalance().compareTo(senderFinalBalance) == 0);
+		assertTrue(receiver.getBalance().compareTo(receiverFinalBalance) == 0);
 	}
-	
+
 	@Test
 	@DisplayName("Should throw exception if any user does not exist when adding the transaction")
-	public void testAddTransactionFailure() throws Exception {
-		
+	public void testAddTransactionUserDoesNotExistFailure() throws Exception {
+
 		TransactionDTO transactionDTO = new TransactionDTO();
 		transactionDTO.setAmount(new BigDecimal(500));
 		transactionDTO.setSenderDocument("33333333333");
 		transactionDTO.setReceiverDocument("111111111");
-		
+
 		Optional<User> initialSender = userRepository.findUserByDocument(transactionDTO.getSenderDocument());
-	    Optional<User> initialReceiver = userRepository.findUserByDocument(transactionDTO.getReceiverDocument());
-	    
-	    assertFalse(initialSender.isPresent());
-	    
-	    assertThrows(EntityNotPresentException.class, () -> transactionService.addTransaction(transactionDTO));
+		Optional<User> initialReceiver = userRepository.findUserByDocument(transactionDTO.getReceiverDocument());
+
+		assertFalse(initialSender.isPresent());
+
+		assertThrows(EntityNotPresentException.class, () -> transactionService.addTransaction(transactionDTO));
 	}
-	
+
+	@Test
+	@DisplayName("Should throw exception if sender user type is shopkeeper when adding the transaction")
+	public void testAddTransactionSenderUserTypeInvalidFailure() throws Exception {
+
+		TransactionDTO transactionDTO = new TransactionDTO();
+		transactionDTO.setAmount(new BigDecimal(500));
+		transactionDTO.setSenderDocument("333333333");
+		transactionDTO.setReceiverDocument("222222222");
+
+		User sender = userRepository.findUserByDocument(transactionDTO.getSenderDocument()).get();
+		User receiver = userRepository.findUserByDocument(transactionDTO.getReceiverDocument()).get();
+
+		assertThat(sender).isNotNull();
+		assertThat(receiver).isNotNull();
+		
+		assertTrue(sender.getUserType().equals(UserType.SHOPKEEPER));
+
+		assertThrows(SenderUserTypeInvalidException.class, () -> transactionService.addTransaction(transactionDTO));
+	}
+
 }
