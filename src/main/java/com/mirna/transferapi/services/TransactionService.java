120d1703a199afe73a23c1ba62b105e21a899a13
Mirna Gama <mirnabeatrizgamav@gmail.com>
2023-12-11 12:31:48 -0300

    [R2] Transaction service class and add transaction method

---
+++ src/main/java/com/mirna/transferapi/services/TransactionService.java
@@ -0,0 +1,47 @@
+package com.mirna.transferapi.services;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mirna.transferapi.domain.dtos.TransactionDTO;
+import com.mirna.transferapi.domain.entities.Transaction;
+import com.mirna.transferapi.domain.entities.User;
+import com.mirna.transferapi.exceptions.EntityNotPresentException;
+import com.mirna.transferapi.repositories.TransactionRepository;
+
+@Service
+public class TransactionService {
+
+	@Autowired
+	private TransactionRepository transactionRepository;
+
+	@Autowired
+	private UserService userService;
+
+	public Transaction addTransaction(TransactionDTO transactionDTO) throws EntityNotPresentException {
+
+		Transaction transaction = new Transaction();
+
+		User sender = userService.fetchUser(transactionDTO.getSenderDocument());
+		User receiver = userService.fetchUser(transactionDTO.getReceiverDocument());
+
+		transaction.setSender(sender);
+		transaction.setReceiver(receiver);
+		transaction.setAmount(transactionDTO.getAmount());
+		transaction.setTimestamp(LocalDateTime.now());
+
+		BigDecimal newBalanceSender = sender.getBalance().subtract(transaction.getAmount());
+		sender.setBalance(newBalanceSender);
+
+		BigDecimal newBalanceReceiver = receiver.getBalance().add(transaction.getAmount());
+		receiver.setBalance(newBalanceReceiver);
+
+		userService.updateUser(sender, sender.getDocument());
+		userService.updateUser(receiver, receiver.getDocument());
+
+		return transactionRepository.save(transaction);
+	}
+}
