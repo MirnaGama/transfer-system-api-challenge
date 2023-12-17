d3ef2b691f42d1ee443cf747897303f0d75aad01
Mirna Gama <mirnabeatrizgamav@gmail.com>
2023-12-17 19:32:55 -0300

    [R5] Use authorization service and add new method just for validations

--- src/main/java/com/mirna/transferapi/services/TransactionService.java
+++ src/main/java/com/mirna/transferapi/services/TransactionService.java
@@ -13,6 +13,7 @@ import com.mirna.transferapi.domain.enums.UserType;
 import com.mirna.transferapi.exceptions.EntityNotPresentException;
 import com.mirna.transferapi.exceptions.InsufficientBalanceException;
 import com.mirna.transferapi.exceptions.SenderUserTypeInvalidException;
+import com.mirna.transferapi.exceptions.UnauthorizedTransactionException;
 import com.mirna.transferapi.repositories.TransactionRepository;
 
 @Service
@@ -23,36 +24,55 @@ public class TransactionService {
 
 	@Autowired
 	private UserService userService;
+	
+	@Autowired
+	private AuthorizationService authorizationService;
 
-	public Transaction addTransaction(TransactionDTO transactionDTO) throws EntityNotPresentException, SenderUserTypeInvalidException, InsufficientBalanceException {
+	public Transaction addTransaction(TransactionDTO transactionDTO) throws EntityNotPresentException, SenderUserTypeInvalidException, InsufficientBalanceException, UnauthorizedTransactionException {
 
 		Transaction transaction = new Transaction();
 
 		User sender = userService.fetchUser(transactionDTO.getSenderDocument());
 		User receiver = userService.fetchUser(transactionDTO.getReceiverDocument());
-		
-		BigDecimal senderFinalBalance = sender.getBalance().subtract(transactionDTO.getAmount());
-		
-		if (senderFinalBalance.compareTo(BigDecimal.ZERO) < 0) {
-			throw new InsufficientBalanceException();
-		} else if (sender.getUserType().equals(UserType.SHOPKEEPER)) {
-			throw new SenderUserTypeInvalidException();
-		} 
 
 		transaction.setSender(sender);
 		transaction.setReceiver(receiver);
 		transaction.setAmount(transactionDTO.getAmount());
 		transaction.setTimestamp(LocalDateTime.now());
+		
+		validateTransaction(sender, transaction);
 
 		BigDecimal newBalanceSender = sender.getBalance().subtract(transaction.getAmount());
 		sender.setBalance(newBalanceSender);
 
 		BigDecimal newBalanceReceiver = receiver.getBalance().add(transaction.getAmount());
 		receiver.setBalance(newBalanceReceiver);
+		
+		if (!authorizationService.isTransactionAuthorized()) {
+			throw new UnauthorizedTransactionException();
+		}
 
 		userService.updateUser(sender, sender.getDocument());
 		userService.updateUser(receiver, receiver.getDocument());
 
 		return transactionRepository.save(transaction);
 	}
+	
+	private void validateTransaction(User sender, Transaction transaction) throws UnauthorizedTransactionException, SenderUserTypeInvalidException, InsufficientBalanceException {
+		
+		if (sender.getUserType().equals(UserType.SHOPKEEPER)) {
+			throw new SenderUserTypeInvalidException();
+		}
+		
+		BigDecimal senderFinalBalance = sender.getBalance().subtract(transaction.getAmount());
+		
+		if (senderFinalBalance.compareTo(BigDecimal.ZERO) < 0) {
+			throw new InsufficientBalanceException();
+		}
+			
+		if (!authorizationService.isTransactionAuthorized()) {
+			throw new UnauthorizedTransactionException();
+		}
+	}
+	
 }
