d104c8d30a27f5e9e3a474854756d4eba078b2ad
Mirna Gama <mirnabeatrizgamav@gmail.com>
2023-12-17 19:32:55 -0300

    [R5] Use authorization service and add new method just for validations

---
+++ src/main/java/com/mirna/transferapi/exceptions/UnauthorizedTransactionException.java
@@ -0,0 +1,9 @@
+package com.mirna.transferapi.exceptions;
+
+public class UnauthorizedTransactionException extends Exception {
+
+	public UnauthorizedTransactionException() {
+	super("Unauthorized Transaction");
+	}
+	
+}
